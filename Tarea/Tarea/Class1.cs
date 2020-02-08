9c6052c7ae685d770eb2ee43fd7b58434ead109f
danixut <danixut10.000@gmail.com>
2020-02-08 12:39:00 -0600

    Tarea2

---
+++ Tarea/Tarea/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea
+{
+    class Alumno
+    {
+        public string nombre;
+        public string apPaterno;
+        public int calProyecto;
+        public int numTareas;
+        public int numParticipaciones;
+
+        public Alumno(string Nombre, string ApPat, int CalP, int NumT, int NumP)
+        {
+            this.nombre = Nombre;
+            this.apPaterno = ApPat;
+            this.calProyecto = CalP;
+            this.numTareas = NumT;
+            this.numParticipaciones = NumP;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Hola me llamo " + nombre);
+        }
+    }
+}
---
+++ Tarea/Tarea/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Tarea
+{
+    class Program
+    {
+
+        static double Calculadora(int CalP, int NumT, int NumP)
+        {
+            double punProyecto = CalP * 0.6;
+            double punTareas = 0;
+            bool Participaciones = false;
+            double punParticipacion = 0.8;
+            double CaliFinal;            
+                if (NumT == 5)
+                    punTareas = 4;
+                else
+                    punTareas = (NumT * 4) / 5;            
+            if (NumP < 5)
+            {
+                Participaciones = true;
+            }
+            if (Participaciones)
+                CaliFinal = punProyecto + punTareas + punParticipacion;
+            else
+                CaliFinal = punProyecto + punTareas;
+            if (CaliFinal > 10)
+                return 10;
+            else
+                return CaliFinal;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el numero de alumnos a calificar");
+            int numAl = Convert.ToInt32(Console.ReadLine());
+            Alumno[] alumnos = new Alumno[numAl];
+
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                Console.WriteLine("Ingrese el nombre del Alumno:");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el apellido del Alumno:");
+                string apPaterno = Console.ReadLine();
+                Console.WriteLine("Ingrese la calificacion del Proyecto:");
+                int calProyecto = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el numero de Tareas:");
+                int numTareas = Convert.ToInt32(Console.ReadLine());
+                if (numTareas > 5)
+                {                    
+                        Console.WriteLine("Las tareas ingresadas son mas de las que deberian");
+                        Console.WriteLine("Ingrese el numero de Tareas:");
+                        numTareas = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("Ingrese el numero de Participaciones:");
+                int numParticipaciones = Convert.ToInt32(Console.ReadLine());
+                alumnos[i] = new Alumno(nombre, apPaterno, calProyecto, numTareas, numParticipaciones);
+            }
+            double calificacion;
+            foreach (var alumno in alumnos)
+            {
+                calificacion = Calculadora(alumno.calProyecto,alumno.numTareas,alumno.numParticipaciones);
+                Console.WriteLine("La calificiacion de "+alumno.nombre+" es: {0:0.00}",calificacion);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
